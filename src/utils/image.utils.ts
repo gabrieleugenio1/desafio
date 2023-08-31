b2c39d612ba5a605546ed1a094327cfd75a8b26f
gabrieleugenio1 <101233631+gabrieleugenio1@users.noreply.github.com>
2023-08-30 21:53:30 -0300

    refactor(docs): update comments

--- src/utils/image.utils.ts
+++ src/utils/image.utils.ts
@@ -4,24 +4,30 @@ import * as mime from 'mime-types';
 import { BadRequestException } from '@nestjs/common';
 
 export async function saveImage(image: Express.Multer.File): Promise<string> {
+  // Get the mime type of the image
   const mimeType = mime.lookup(image.originalname);
 
+  // Check if image size is less than 10MB
   if (image.size > 10 * 1024 * 1024) {
     throw new BadRequestException('Image size exceeds 10MB');
   }
+
+  // Check if file is an image (png, jpg, jpeg, gif)
   if (!mimeType || !mimeType?.startsWith('image/')) {
     throw new BadRequestException('File is not an image.');
   }
 
   const timestamp = Date.now();
+  // Create a filename with timestamp and original name
   const filename = `${timestamp}_${image.originalname}`;
+  // Create the path to save the image
   const imagePath = path.join('./uploads', filename);
 
-  // Verificar se o diretório de destino existe, se não existir, criar
+  // Verify if the destination directory exists, if not, create
   const directory = path.dirname(imagePath);
   await fs.mkdir(directory, { recursive: true });
 
-  // Salvar a imagem no diretório
+  // Save the image in the uploads folder
   await fs.writeFile(imagePath, image.buffer);
   return imagePath;
 }
