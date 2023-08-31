cd574bf11f75a22d5cc75b0ab18b37a13c609d68
gabrieleugenio1 <101233631+gabrieleugenio1@users.noreply.github.com>
2023-08-31 02:27:52 -0300

    fix(vercel): display Swagger UI on Vercel

--- src/main.ts
+++ src/main.ts
@@ -11,10 +11,22 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    // Show Swagger UI
+    customSiteTitle: 'Products',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.4.2/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.4.2/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.4.2/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.4.2/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.4.2/swagger-ui.css',
+    ],
+  });
 
   app.useGlobalFilters(new NotFoundExceptionFilter());
-
+ 
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
